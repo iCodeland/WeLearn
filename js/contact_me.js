74950f2ce9d8a7f4754415faf2eabb5973299379
Yi-Cheng Chen <chad.cyb@gmail.com>
2018-07-11 07:40:22 +0800

    update icon and css style

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,8 +10,7 @@ $(function() {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
+      var phone = $("input#passwd").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
